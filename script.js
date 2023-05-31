6b285ae137f3642807ff6f5473b46d28a1764599
Elher Encarnacion <encarnacionelher@gmail.com>
2023-06-01 07:57:16 +0800

    change hola to hej

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed2 = new Typed('#greetings', {
-  strings: ["<span style='color:rgb(34, 211, 238)'>Hola,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>"],
+  strings: ["<span style='color:rgb(34, 211, 238)'>Hej,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>"],
   typeSpeed: 60,
   backSpeed: 50,
   showCursor: false,
