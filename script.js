a97ea97dd856bb6e47d7554d628e885249973576
Elher Encarnacion <encarnacionelher@gmail.com>
2023-05-31 17:26:50 +0800

    remove old and added new portf

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed2 = new Typed('#greetings', {
+  strings: ["<span style='color:rgb(34, 211, 238)'>Hola,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>"],
+  typeSpeed: 60,
+  backSpeed: 50,
+  showCursor: false,
+  loop: true
+});
+
+
