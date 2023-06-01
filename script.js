43bd7f479074fde031a3636f83fc83dc61c22dc1
Elher Encarnacion <encarnacionelher@gmail.com>
2023-06-01 17:33:47 +0800

    added links and hover on dl button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var typed2 = new Typed('#greetings', {
-  strings: ["<span style='color:rgb(34, 211, 238)'>Hej,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>"],
-  typeSpeed: 60,
+  strings: ["<span style='color:rgb(34, 211, 238)'>Hej,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>","<span style='color:rgb(52, 211, 153)'>Hola,</span>","<span style='color:rgb(251, 113, 133)'>Ciao,</span>","<span style='color:rgb(250, 204, 21)'>Hallo,</span>"],
+  typeSpeed: 80,
   backSpeed: 50,
   showCursor: false,
   loop: true
