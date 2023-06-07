5e86211b902712020bc717a626417e35ffd4e739
Elher Encarnacion <encarnacionelher@gmail.com>
2023-06-07 10:23:51 +0800

    added loading screen

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#loading").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#loading").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
+
+
+
+
 var typed2 = new Typed('#greetings', {
   strings: ["<span style='color:rgb(34, 211, 238)'>Hej,</span>", "<span style='color:rgb(250, 204, 21)'>Bonjour,</span>",  "<span style='color:rgb(74, 222, 128))'>Hello,</span>","<span style='color:rgb(52, 211, 153)'>Hola,</span>","<span style='color:rgb(251, 113, 133)'>Ciao,</span>","<span style='color:rgb(250, 204, 21)'>Hallo,</span>"],
   typeSpeed: 80,
@@ -5,5 +22,8 @@ var typed2 = new Typed('#greetings', {
   showCursor: false,
   loop: true
 });
+
+
+
 
 
