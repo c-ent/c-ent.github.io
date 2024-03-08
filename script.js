8c7d1b920888a0373223d71c2893fdaf25fd2ea5
Elher Encarnacion <encarnacionelher@gmail.com>
2024-03-08 23:47:20 +0800

    adec csutom mouse

--- script.js
+++ script.js
@@ -10,7 +10,62 @@ document.onreadystatechange = function() {
       document.querySelector(
         "body").style.visibility = "visible";
   }
+
+ 
 };
+
+// cursor options
+let acOptions = {
+  color: '#fff',
+  outerAlpha: 0.4,
+  size: {
+    inner: 8,
+    outer: 38
+  },
+  hoverScale: {
+    mixBlendMode: 'exclusion',
+    inner: 0.5,
+    outer: 1.4
+  },
+  clickScale: {
+    inner: 1.4,
+    outer: 0.1,
+    mixBlendMode: 'exclusion'
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+let blendOpts = {
+  cursorInnerSelector: '#cursor-inner',
+  cursorOuterSelector: '#cursor-outer',
+  cursorInnerStyles: {
+    backgroundColor: '#fff',
+    mixBlendMode: 'exclusion'
+  },
+  cursorOuterStyles: {
+    backgroundColor: 'var(--cursor-color)',
+    mixBlendMode: 'exclusion'
+  },
+  size: {
+    inner: 8,
+    outer: 35
+  },
+  hoverScale: {
+    inner: 0.5,
+    outer: 1.7
+  },
+  clickScale: {
+    inner: 1.4,
+    outer: 0.1
+  }
+}
+
+const ac = window.animatedCursor(acOptions)
+
+ac.init() 
+});
 
 
 
@@ -22,6 +77,9 @@ var typed2 = new Typed('#greetings', {
   showCursor: false,
   loop: true
 });
+
+
+
 
 
 
